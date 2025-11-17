b068c34077d7d3dcbe02bdd6aa4f335ba23dc4c9
Kyoko Kikoku <mama.kanata0417@gmail.com>
2025-11-17 21:20:51 +0900

    wedをアップロード

---
+++ codejump-WED/header.php
@@ -0,0 +1,31 @@
+<html lang="ja">
+  <head>
+    <title>WED</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <!-- ヘッダー -->
+    <header class="header">
+      <div class="header-inner">
+        <div class="header-set">
+          <h1 class="logo">
+            <a href="index.html"><img src="img/logo.svg" alt="WED"></a>
+          </h1>
+          <nav class="nav">
+            <ul class="nav-inner">
+              <li class="nav-list"><a href="news.html">NEWS</a></li>
+              <li class="nav-list"><a href="index.html">ABOUT</a></li>
+              <li class="nav-list"><a href="index.html">BUSINESS</a></li>
+              <li class="nav-list"><a href="index.html">COMPANY</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div class="contact-btn">
+          <a href="contact.html">お問い合わせ</a>
+        </div>
+      </div>
+    </header>
